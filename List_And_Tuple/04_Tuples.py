30e8f9118114e1735fbd3df93fc1af9344a9479c
Janmesh2003 <someshthakur229@gmail.com>
2024-06-19 15:51:13 +0530

    Update 04_Tuples.py

--- List_And_Tuple/04_Tuples.py
+++ List_And_Tuple/04_Tuples.py
@@ -1,10 +1,11 @@
-##                      luples In python
+##                      Tuples In python
 ##   A built-in data type that lets us create immutable sequences of values.
+
 
 tup = (87, 64, 33, 95, 76)       # tup[0], tup [1]
 # tup [ 0 ] = 43                 # NOT allowed in python
 
-# print ( tup [0] )                # O/p => 87
+# print ( tup [0] )               # O/p => 87
 # print ( tup )
 
 # print ( type ( tup ) )           # O/p => <class 'tuple'>
@@ -19,42 +20,15 @@ print ( type ( tup1 ) )         # O/p => <class 'int'>
 
 tup2 = ( 3, )                   # this is right way to declare single tuple           
 
-print (tup2)
+print ( tup2 )
 
 print ( type ( tup2 ) )         # O/p => <class 'tuple'>
 
 
 #--------------------------## slicing in tuple ##---------------------------#
 
+
 tup3 = (1, 3, 4, 34, 0, 5)
 
 print ( tup3 [ 1 : 4 ] )        # Slicing Same hai String or list jaysi 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
