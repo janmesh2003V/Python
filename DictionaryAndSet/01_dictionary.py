6c64ee321b31bea5b89d242b57c68616639aed1a
Janmesh2003 <someshthakur229@gmail.com>
2024-07-09 16:31:15 +0530

    Update 01_dictionary.py

--- DictionaryAndSet/01_dictionary.py
+++ DictionaryAndSet/01_dictionary.py
@@ -12,34 +12,33 @@ dict = {
 
 }
 
-# print ( dict )                # O/p => {'name': 'shradha', "'cgpa": 9.6, 'marks': [98, 97, 95], 'friends': ['shubham', 'saurabh', 'rahul']}
-# print ( type ( dict ) )       # O/p => <class 'dict'>
+print ( dict )                            # O/p => {'name': 'shradha', "'cgpa": 9.6, 'marks': [98, 97, 95], 'friends': ['shubham', 'saurabh', 'rahul']}
+print ( type ( dict ) )                   # O/p => <class 'dict'>
 
-# print ( dict ["name"] )       # O/p => shradha  yeh key ki value ko print karta hai
-# print ( dict ["friends"] )    # O/p => ['shubham', 'saurabh', 'rahul']
+print ( dict ["name"] )                   # O/p => shradha  yeh key ki value ko print karta hai
+print ( dict ["friends"] )                # O/p => ['shubham', 'saurabh', 'rahul']
 
 ## Abh mai "name" key ki value change karunga 
 
 dict ["name"] = "janmesh"
-# print (dict ["name"] )  
+print (dict ["name"] )  
 
 ## Abh new key add karunga Dictiionary mai
 
-dict ["SurName"] = "thakur"   # last mai SurName key add ho gyi or uski value bhi 
-# print ( dict )
+dict ["SurName"] = "thakur"               # last mai SurName key add ho gyi or uski value bhi 
+print ( dict )
 
 
 #---------------------------------------------------------------#
 
-dict2 = {}                  # yeh null dictionary hum iske ander key : value daal sathte hai or print bhi kar sakte hai 
-
-# print (dict2)
+dict2 = {}                            # yeh null dictionary hum iske ander key : value daal sathte hai or print bhi kar sakte hai 
+print (dict2)
 
 ## Add new key and value in null dict2 
 
 dict2 ["NAME"] = "somesh"   
 dict2 ["day"]  = "new "         
-# print ( dict2 )           # O/p => {'NAME': 'somesh', 'day': 'new '}
+print ( dict2 )                       # O/p => {'NAME': 'somesh', 'day': 'new '}
 
 
 #--------------------------------# Nested dictionary #----------------------------------#
@@ -61,7 +60,7 @@ student = {
 
 #nested dictionary
 
-print ( student )           # O/p => {'name': 'rahul kumar', 'subjects': {'phy': 97, 'chem': 98, 'math': 95}, 'clg': 'mit'}
+print ( student )                       # O/p => {'name': 'rahul kumar', 'subjects': {'phy': 97, 'chem': 98, 'math': 95}, 'clg': 'mit'}
 
 print ( student [ "subjects" ] )        # O/p => {'phy': 97, 'chem': 98, 'math': 95}
 
