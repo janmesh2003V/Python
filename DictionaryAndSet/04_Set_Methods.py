08e13b8f92eb7d3591abc7a86e824862e4668a32
Janmesh2003 <someshthakur229@gmail.com>
2024-07-09 17:07:56 +0530

    Update 04_Set_Methods.py

--- DictionaryAndSet/04_Set_Methods.py
+++ DictionaryAndSet/04_Set_Methods.py
@@ -1,56 +1,56 @@
-#----------------------------------------# 1. add("Element") #-------------------------------------------#
+----------------------------------------# 1. add("Element") #-------------------------------------------#
 
 
-# Set1 = { 1, 2, 3, 4, 5 }
+Set1 = { 1, 2, 3, 4, 5 }
 
-# Set1.add ( 22 )
-# # Set1.add ( 33, 10 )           # yeh Error dega, TypeError: set.add() takes exactly one argument (2 given)
-# Set1.add ( 55 )
+Set1.add ( 22 )
+Set1.add ( 33, 10 )             # yeh Error dega, TypeError: set.add() takes exactly one argument (2 given)
+Set1.add ( 55 )
 
-# print ( Set1 )                  # O/p => {1, 2, 3, 4, 5, 22, 55}
+print ( Set1 )                  # O/p => {1, 2, 3, 4, 5, 22, 55}
 
-# # tuple bhi Add kar sakte hai
+# tuple bhi Add kar sakte hai
 
-# Set1.add (( 55, 555, 5555, 55555 ))
+Set1.add (( 55, 555, 5555, 55555 ))
 
-# print (Set1)
-
-
-#----------------------------------------# 2. remove("Element") #---------------------------------------#
+print ( Set1 )                    # O/p => {1, 2, 3, 4, 5, 22, 55, (55, 555, 5555, 55555)} 
 
 
-# Set2 = { 1, 2, 3, 4, 5 }
-
-# Set2.remove ( 3 )
-# # Set2.remove ( 3 )                # yeh bhi error hai Ek hi value ko secod time remove nhi code kar sakte hai
-
-# # Set2.remove ( 7 )                # yeh bhi Error hai remove function mein Set ke element se alag value nhi de sakte hai  
-# Set2.remove ( 4 )
-
-# print ( Set2 )                    # O/p => {1, 2, 5}
+----------------------------------------# 2. remove("Element") #---------------------------------------#
 
 
-#----------------------------------------# 3. clear() #-----------------------------------------------#
-## yeh clear karta hai set ko
+Set2 = { 1, 2, 3, 4, 5 }
+
+Set2.remove ( 3 )
+# Set2.remove ( 3 )                # yeh bhi error hai Ek hi value ko secod time remove nhi code kar sakte hai
+
+# Set2.remove ( 7 )                # yeh bhi Error hai remove function mein Set ke element se alag value nhi de sakte hai  
+Set2.remove ( 4 )
+
+print ( Set2 )                     # O/p => {1, 2, 5}
 
 
-# Set3 = { 5, 10, 15, 20, 50 }
-
-# Set3.clear()
-
-# print ( Set3 )
-# print ( len ( Set3 ) )              # O/p => 0
+----------------------------------------# 3. clear() #-----------------------------------------------#
+# yeh clear karta hai set ko
 
 
-#-----------------------------------------# 4. pop() #------------------------------------------------# 
-## removes a random value
-## yeh set ko clear nhi karta hai, yeh ek value ko remove karta``
+Set3 = { 5, 10, 15, 20, 50 }
+
+Set3.clear()
+
+print ( Set3 )
+print ( len ( Set3 ) )              # O/p => 0
 
 
-# Set4 = { 5, 10, 15, 31, 20, 50 }
+-----------------------------------------# 4. pop() #------------------------------------------------# 
+# removes a random value
+# yeh set ko clear nhi karta hai, yeh ek value ko remove karta``
 
-# Set4.pop()
-# print ( Set4 )                       # O/p => {20, 5, 10, 31, 15}  " Yeh koi  bhi rendom value ko reove kar deta hai "
+
+Set4 = { 5, 10, 15, 31, 20, 50 }
+
+Set4.pop()
+print ( Set4 )                       # O/p => {20, 5, 10, 31, 15}  " Yeh koi  bhi rendom value ko reove kar deta hai "
 
 
 #-----------------------------------------# 5. union() #----------------------------------------------#
@@ -63,9 +63,11 @@ Set6 = {"i", "am", "janmesh", "thakur", "python", "developer"}
 
 
 print ( Set5.union ( Set6 ) )       # yeh "union" function dono Set ko combine karke print karta hai or duplicate value ko Ek hi baar print kart hai
+# O/p => {'janmesh', 'python', 'i', 'am', 'thakur', 'developer'}
 
 
 #----------------------------------------# 6.intersection() #-----------------------------------------#
 
 
 print ( Set5.intersection ( Set6 ) )    # yeh "intersection" function  dono sets mai se common value ko hi print karta hai 
+# O/p => {'janmesh', 'i', 'am', 'thakur'}
