caca9b11a25249a083340e45cafc465c63aeb05f
Janmesh2003 <someshthakur229@gmail.com>
2024-07-09 17:25:47 +0530

    Update 01_while.py

--- Loops/01_while.py
+++ Loops/01_while.py
@@ -2,12 +2,12 @@
 # Loops are used to repeat instructions.
 
 
-# count = 1
+count = 1
 
-# while count <= 5:
+while count <= 5:
     
-#     print(count)
-#     count += 1
+    print(count)
+    count += 1
 
 # reverse 1 to 5
 
@@ -43,36 +43,36 @@ while i2 >= 1:
     i2 -= 1
 
 
-#----------------------------------------------------------------------------------------------------#
+----------------------------------------------------------------------------------------------------#
 ## Print the multiplication table of a number n.
 
 
-# user = int ( input ("Enter nuber for table : ") )
+user = int ( input ("Enter nuber for table : ") )
 
-# i3 = 1
-# while i3 <= 10:
-#     x = user * i3
-#     # print( x )
-#     i3 += 1
+i3 = 1
+while i3 <= 10:
+    x = user * i3
+    # print( x )
+    i3 += 1
 
 
-#----------------------------------------------------------------------------------------------------#
+----------------------------------------------------------------------------------------------------#
 ## Print the elements of the following list using a loop:
 ## [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 
 
-# num1 = [ 1, 4, 9, 16, 25, 36, 49, 64, 81, 100 ]
+num1 = [ 1, 4, 9, 16, 25, 36, 49, 64, 81, 100 ]
 
-# i4 = 0
+i4 = 0
 
-# while ( i < len ( num1 ) ):
-#     print( num1[i] )
-#     i += 1
+while ( i < len ( num1 ) ):
+    print( num1[i] )
+    i += 1
 
 
-#----------------------------------------------------------------------------------------------------#
-# Search for a number x in this tuple using loop:
-# [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+----------------------------------------------------------------------------------------------------#
+## Search for a number x in this tuple using loop:
+[1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 
 
 X = int ( input ("Enter number, which one want to find in tuple : ") )
